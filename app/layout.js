c09fc20511360358de8b3d3795b2ce8310505fad
developer-illford <149462968+developer-illford@users.noreply.github.com>
2024-03-06 11:17:39 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -145,6 +145,21 @@ const RootLayout = ({ children }) => {
       metaTag.setAttribute('href', content || '');
     };
 
+
+
+    (function (w, d, s, l, i) {
+      w[l] = w[l] || []; w[l].push({
+        'gtm.start':
+          new Date().getTime(), event: 'gtm.js'
+      }); var f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
+          'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', 'GTM-MB6BSFND');
+
+
+
+
+
     handleMetaTag('keywords', metadata.keywords);
     handleMetaTag('language', metadata.language);
     handleMetaTag('rating', metadata.rating);
